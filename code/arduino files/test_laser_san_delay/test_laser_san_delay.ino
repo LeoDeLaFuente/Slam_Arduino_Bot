3ef3a7661416eb7c528e62366b7f8ab9166f4ee1
Félix <felix.fonteneau@etu.unice.fr>
2018-01-10 18:26:02 +0100

    miaou

--- code/arduino files/test_laser_san_delay/test_laser_san_delay.ino
+++ code/arduino files/test_laser_san_delay/test_laser_san_delay.ino
@@ -1,20 +1,19 @@
 #include <SoftwareSerial.h>
 SoftwareSerial Laser(8,9); //RX TX
-char b = 'F';
-char c = 'F';
+char c; 
 boolean passe = true;
-boolean sort = false;
 long chrono = 0;
 
 void setup() {
   Serial.begin(19200);
   Laser.begin(19200);
   delay(3000);
-  Serial.println("Arduino A : je suis pret a recsvoir/emmettre");
+  Serial.println("Arduino 1 : je suis pret a recevoir/emmettre");
 }
 
 void loop(){
- //*
+ /*dans ce loop, on va effectuer la demande de mesure avec le char F
+ puis attendre minimum 0,310 secondes, et récuper les char avec le while*/
  if(millis()-chrono>310){
     if(Laser.available() && !passe){
       while( c != '\n'){
@@ -31,8 +30,5 @@ void loop(){
     }
  chrono=millis();   
  }
- 
- //*/
-  
 }
 
