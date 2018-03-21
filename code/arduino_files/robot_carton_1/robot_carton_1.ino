6ac3d3bba18c8ed49dd33f5830b3643937bd3a72
LeoDeLaFuente <LeoDeLaFuente@users.noreply.github.com>
2018-03-21 21:23:58 +0100

    amélioration code "robot carton"

--- code/arduino_files/robot_carton_1/robot_carton_1.ino
+++ code/arduino_files/robot_carton_1/robot_carton_1.ino
@@ -14,19 +14,24 @@
 // Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.
 
 // Motor 1
-int dir1PinA = 3;
-int dir2PinA = 4;
+int dir1PinA = 4;
+int dir2PinA = 5;
 int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
 
 // Motor 2
-int dir1PinB = 5;
-int dir2PinB = 6;
+int dir1PinB = 6;
+int dir2PinB = 7;
 int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
 
-int speed=0;
-int inByte = 0;
+int speed_A=0;
+int speed_B=0;
 
-unsigned int tick_codeuse = 0; 
+int inByte_A = 0;
+int inByte_B = 0;
+
+unsigned int tick_codeuse_A = 0; 
+unsigned int tick_codeuse_B = 0; 
+
 int avancer_tick=0;
 
 void setup() {  // Setup runs once per reset
@@ -42,10 +47,11 @@ pinMode(dir1PinB,OUTPUT);
 pinMode(dir2PinB,OUTPUT);
 pinMode(speedPinB,OUTPUT);
 
-attachInterrupt(digitalPinToInterrupt(2), compteur, CHANGE);   // Interruption sur tick de la codeuse (interruption 0 = pin2 arduino mega)
-
-int avancer_cm=705;
-inByte = '1';
+attachInterrupt(digitalPinToInterrupt(2), compteur_A, CHANGE); // Interruption sur tick de la codeuse (interruption 0 = pin2 arduino mega)
+attachInterrupt(digitalPinToInterrupt(3), compteur_B, CHANGE);
+int avancer_cm=150;
+inByte_A = '1';
+inByte_B = '4';
 
 avancer_tick=convert(avancer_cm);
 Serial.println(avancer_tick);
@@ -54,91 +60,137 @@ Serial.println(avancer_tick);
 
 void loop() {
 
-int speed=255;
+int speed_A=255;
+int speed_B=255;
 // Initialize the Serial interface:
 
 
   
   
-  if(avancer_tick-tick_codeuse<35){
-    speed=20;
+  if(avancer_tick-tick_codeuse_A<150){
+    speed_A=10;
   }
-  Serial.println(tick_codeuse);
+  if(avancer_tick-tick_codeuse_B<150){
+    speed_B=10;
+  }
+  Serial.print(tick_codeuse_A);
+  Serial.print(" | ");
+  Serial.println(tick_codeuse_B);
+
+  
  //Serial.println(avancer_tick);
-if(tick_codeuse<avancer_tick){
-  //Serial.println(inByte);
-switch (inByte) {
+if(tick_codeuse_A<avancer_tick){
+  
+  switch (inByte_A) {
+  
+  //______________Motor 1______________
+  
+  case '1': // Fait avancer les deux moteurs dans les sens de la marche
+  analogWrite(speedPinA, speed_A);//Sets speed variable via PWM 
 
-//______________Motor 1______________
-
-case '1': // Fait avancer les deux moteurs dans les sens de la marche
-analogWrite(speedPinA, speed);//Sets speed variable via PWM 
-analogWrite(speedPinB, speed);
-
-digitalWrite(dir1PinA, LOW);
-digitalWrite(dir2PinA, HIGH);
-
-digitalWrite(dir1PinB, LOW);
-digitalWrite(dir2PinB, HIGH);
-
-//Serial.println("Motor 1 Forward"); // Prints out “Motor 1 Forward” on the serial monitor
-//Serial.println("    "); // Creates a blank line printed on the serial monitor
-break;
-
-case '2': // Motor 1 Stop (Freespin)
-analogWrite(speedPinA, 0);
-digitalWrite(dir1PinA, LOW);
-digitalWrite(dir2PinA, HIGH);
-Serial.println("Motor 1 Stop");
-Serial.println("    ");
-break;
-
-case '3': // Motor 1 Reverse
-analogWrite(speedPinA, speed);
-digitalWrite(dir1PinA, HIGH);
-digitalWrite(dir2PinA, LOW);
-Serial.println("Motor 1 Reverse");
-Serial.println("    ");
-break;
-
-//______________Motor 2______________
-
-case '4': // Motor 2 Forward
-
-
-Serial.println("Motor 2 Forward");
-Serial.println("    ");
-break;
-
-case '5': // Motor 1 Stop (Freespin)
-analogWrite(speedPinB, 0);
-digitalWrite(dir1PinB, LOW);
-digitalWrite(dir2PinB, HIGH);
-Serial.println("Motor 2 Stop");
-Serial.println("    ");
-break;
-
-case '6': // Motor 2 Reverse
-analogWrite(speedPinB, speed);
-digitalWrite(dir1PinB, HIGH);
-digitalWrite(dir2PinB, LOW);
-Serial.println("Motor 2 Reverse");
-Serial.println("    ");
-break;
-
-default:
-// turn all the connections off if an unmapped key is pressed:
-for (int thisPin = 2; thisPin < 11; thisPin++) {
-digitalWrite(thisPin, LOW);
-}
+  
+  digitalWrite(dir1PinA, LOW);
+  digitalWrite(dir2PinA, HIGH);
+  
+ 
+  
+  //Serial.println("Motor 1 Forward"); // Prints out “Motor 1 Forward” on the serial monitor
+  //Serial.println("    "); // Creates a blank line printed on the serial monitor
+  break;
+  
+  case '2': // Motor 1 Stop (Freespin)
+  analogWrite(speedPinA, 0);
+  digitalWrite(dir1PinA, LOW);
+  digitalWrite(dir2PinA, HIGH);
+  Serial.println("Motor 1 Stop");
+  Serial.println("    ");
+  break;
+  
+  case '3': // Motor 1 Reverse
+  analogWrite(speedPinA, speed_A);
+  digitalWrite(dir1PinA, HIGH);
+  digitalWrite(dir2PinA, LOW);
+  Serial.println("Motor 1 Reverse");
+  Serial.println("    ");
+  break;
+  
+  //______________Motor 2______________
+  
+  case '4': // Motor 2 Forward
+  
+  
+  Serial.println("Motor 2 Forward");
+  Serial.println("    ");
+  break;
+  
+  case '5': // Motor 1 Stop (Freespin)
+  analogWrite(speedPinB, 0);
+  digitalWrite(dir1PinB, LOW);
+  digitalWrite(dir2PinB, HIGH);
+  Serial.println("Motor 2 Stop");
+  Serial.println("    ");
+  break;
+  
+  case '6': // Motor 2 Reverse
+  analogWrite(speedPinB, speed_B);
+  digitalWrite(dir1PinB, HIGH);
+  digitalWrite(dir2PinB, LOW);
+  Serial.println("Motor 2 Reverse");
+  Serial.println("    ");
+  break;
+  
+  default:
+  // turn all the connections off if an unmapped key is pressed:
+  for (int thisPin = 2; thisPin < 11; thisPin++) {
+  digitalWrite(thisPin, LOW);
   }
-    }else {analogWrite(speedPinA, 0);analogWrite(speedPinB, 0);}
+    }
+      }
+else {
+  analogWrite(speedPinA, 0);
+ }
+
+ if(tick_codeuse_B<avancer_tick){
+  
+  switch (inByte_B) {
+  
+  //______________Motor 1______________
+  
+  case '4': // Fait avancer les deux moteurs dans les sens de la marche
+ 
+  analogWrite(speedPinB, speed_B);
+  
+
+  digitalWrite(dir1PinB, LOW);
+  digitalWrite(dir2PinB, HIGH);
+  
+  //Serial.println("Motor 1 Forward"); // Prints out “Motor 1 Forward” on the serial monitor
+  //Serial.println("    "); // Creates a blank line printed on the serial monitor
+  break;
+  
+ 
+  default:
+  // turn all the connections off if an unmapped key is pressed:
+  for (int thisPin = 2; thisPin < 11; thisPin++) {
+  digitalWrite(thisPin, LOW);
+  }
+    }
+      }
+else {
+  
+  analogWrite(speedPinB, 0);
+ }
+      
 }
       
 
 /* Interruption sur tick de la codeuse */
-void compteur(){
-    tick_codeuse=tick_codeuse+1;  // On incrémente le nombre de tick de la codeuse
+void compteur_A(){
+    tick_codeuse_A=tick_codeuse_A+1;  // On incrémente le nombre de tick de la codeuse
+    //Serial.println(tick_codeuse);
+}
+void compteur_B(){
+    tick_codeuse_B=tick_codeuse_B+1;  // On incrémente le nombre de tick de la codeuse
     //Serial.println(tick_codeuse);
 }
 
